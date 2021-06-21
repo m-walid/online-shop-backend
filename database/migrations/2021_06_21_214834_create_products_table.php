6a24ecad109091e788ac02a11aafabc6dab47156
m-walid <49622775+m-walid@users.noreply.github.com>
2021-06-22 00:41:33 +0200

    first commit

---
+++ database/migrations/2021_06_21_214834_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('title');
+            $table->decimal('price');
+            $table->string('description');
+            $table->integer('category_id');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
